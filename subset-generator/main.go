03a68d807e97b50105a5e22d3402a609d8363449
Paul Cager <paul.cager@gmail.com>
2017-05-07 15:30:26 +0100

    Working but memory-heavy

--- subset-generator/main.go
+++ subset-generator/main.go
@@ -2,56 +2,51 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 const (
 	// Max number of items to generate lookup table for. Since the output
 	// is O(2^N), this can't be too high!
-	//limit = 16
-	limit = 5
+	limit = 16
 )
 
 var (
-	buffer  = make([]uint8, 0, 1<<(limit+1))
-	subsets = make([][]struct{ start, end int }, limit+1)
+	subsets = make([][][]uint8, limit+1)
 )
 
 func main() {
 	// subsets([]) == []
-	subsets[0] = []struct{ start, end int }{{0, 0}}
+	subsets[0] = [][]uint8{nil}
 
 	for i := 1; i <= limit; i++ {
 		// subsets([0..N]) = subsets([0..N-1]) ++ eachN(subsets([0..N-1]) ++ [N])
-		subsets[i] = make([]struct{ start, end int }, 0)
+		subsets[i] = make([][]uint8, 0)
 		for j := range subsets[i-1] {
 			subsets[i] = append(subsets[i], subsets[i-1][j])
 		}
 		for _, prev := range subsets[i-1] {
-			l := len(buffer)
-			buffer = append(buffer, buffer[prev.start:prev.end]...)
-			buffer = append(buffer, uint8(i))
-			subsets[i] = append(subsets[i], struct{ start, end int }{l, len(buffer)})
+			buffer := make([]byte, len(prev)+1)
+			copy(buffer, prev)
+			buffer[len(buffer)-1] = uint8(i)
+			subsets[i] = append(subsets[i], buffer)
 		}
 	}
 
-	// TODO -could compress buffer much more: var buffer = []byte{0x1, 0x2, 0x1, 0x2, 0x3, ...
-	fmt.Printf(`package subsets
-// Buffer is %d bytes.
+	f, err := os.Create("../subsets-generated.go")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-var buffer = %#v
-var subsets = [][][]uint8{
-`, len(buffer), buffer)
+	fmt.Fprintf(f, "package subsets\n\n")
+	fmt.Fprintf(f, "// Automatically generated - do not edit\n")
+	fmt.Fprintf(f, "// Limit = %d\n", limit)
 
+	fmt.Fprintf(f, "\nvar subsets = [][][]uint8{\n")
 	for i := range subsets {
-		fmt.Print("\t[][]uint8{")
-		for j := range subsets[i] {
-			if j > 0 {
-				fmt.Print(", ")
-			}
-			fmt.Printf("buffer[%d:%d]", subsets[i][j].start, subsets[i][j].end)
-		}
-		fmt.Println("},")
+		fmt.Fprintf(f, "\t/* % 2d */\t%#v,\n", i, subsets[i])
 	}
 
-	fmt.Println("}")
+	fmt.Fprintf(f, "}\n")
 }
