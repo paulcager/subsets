060100b9f572c2c8a53abd467a2110ee1f5da82f
Paul Cager <paul.cager@gmail.com>
2017-05-07 23:01:35 +0100

    WIP

--- subset-generator/main.go
+++ subset-generator/main.go
@@ -8,7 +8,7 @@ import (
 const (
 	// Max number of items to generate lookup table for. Since the output
 	// is O(2^N), this can't be too high!
-	limit = 16
+	limit = 14
 )
 
 var (
@@ -21,15 +21,15 @@ func main() {
 
 	for i := 1; i <= limit; i++ {
 		// subsets([0..N]) = subsets([0..N-1]) ++ eachN(subsets([0..N-1]) ++ [N])
+		// The previous subsets are not actually stored again.
 		subsets[i] = make([][]uint8, 0)
-		for j := range subsets[i-1] {
-			subsets[i] = append(subsets[i], subsets[i-1][j])
-		}
-		for _, prev := range subsets[i-1] {
-			buffer := make([]byte, len(prev)+1)
-			copy(buffer, prev)
-			buffer[len(buffer)-1] = uint8(i)
-			subsets[i] = append(subsets[i], buffer)
+		for j := 0; j < i; j++ {
+			for _, prev := range subsets[j] {
+				buffer := make([]byte, len(prev)+1)
+				copy(buffer, prev)
+				buffer[len(buffer)-1] = uint8(i)
+				subsets[i] = append(subsets[i], buffer)
+			}
 		}
 	}
 
@@ -45,7 +45,17 @@ func main() {
 
 	fmt.Fprintf(f, "\nvar subsets = [][][]uint8{\n")
 	for i := range subsets {
-		fmt.Fprintf(f, "\t/* % 2d */\t%#v,\n", i, subsets[i])
+		// /*  2 */ [][]uint8{[]uint8{0x2}, []uint8{0x1, 0x2}},
+		// /*  2 */ [][]uint8{{2}, {1, 2}},
+		fmt.Fprintf(f, "\t{ ")
+		for j := range subsets[i] {
+			fmt.Fprintf(f, "{")
+			for k := range subsets[i][j] {
+				fmt.Fprint(f, subsets[i][j][k], ", ")
+			}
+			fmt.Fprintf(f, "}, \n")
+		}
+		fmt.Fprintln(f, "},")
 	}
 
 	fmt.Fprintf(f, "}\n")
